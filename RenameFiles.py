7a0c648d007322c68f3545a8a97ba2c952a33d77
Manuel D <59400729+emdeee@users.noreply.github.com>
2020-10-25 19:24:24 +0100

    Update RenameFiles.py
    now recursevely checks the subdirectories and renames every single file in all subfolders. awesome!

--- RenameFiles.py
+++ RenameFiles.py
@@ -1,20 +1,24 @@
-import os
 import pathlib
 #change the first "\" in the path to a "/" for example C:/bla\blubb
 
-for path in pathlib.Path('C:/Users\manue\Desktop\kekwfolder').iterdir():
-    if path.is_file():
-        contains_digit = False        # only rename the file, if the timestamp is still in the filename, to avoid duplicate namechanges by accident
-        old_name = path.stem
+def rename_files(pathname='C:/Users\manue\Desktop\kekwfolder'):
+    for path in pathlib.Path(pathname).iterdir():
+        if path.is_file():
+            contains_digit = False        # only rename the file, if the timestamp is still in the filename, to avoid duplicate namechanges by accident
+            old_name = path.stem
 
-        for character in old_name:
-            if character.isdigit():
-                contains_digit = True
+            for character in old_name:
+                if character.isdigit():
+                    contains_digit = True
+            
+            if contains_digit:
+                old_extension = path.suffix
+                directory = path.parent
+                new_name =  old_name[6:] + old_extension
+                path.rename(pathlib.Path(directory, new_name))
         
-        if contains_digit:
-            old_extension = path.suffix
-            directory = path.parent
-            new_name =  old_name[6:] + old_extension
-            path.rename(pathlib.Path(directory, new_name))
+        if path.is_dir():
+            rename_files(path)
 
-
+if __name__ == "__main__":
+    rename_files()
